c8efe51093864612dd99772310320a8834697f91
Eklas-Mohin <mohin.030299@gmail.com>
2024-02-12 21:46:35 +0600

    TDPRIMES - Printing some primes

--- printing_some_primes.cpp
+++ printing_some_primes.cpp
@@ -1,33 +1,33 @@
 /**
  *    author: mohin    
 **/
-
+ 
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 const int mx = 1e8;
-vector<bool> sieve(mx + 1, true);
-
-void primeSieve() {
-    sieve[0] = sieve[1] = false;
+vector<bool> prime(mx + 1, true);
+ 
+void sieve() {
+    prime[0] = prime[1] = false;
     for (int i = 4; i <= mx; i += 2) {
-        sieve[i] = false;
+        prime[i] = false;
     }
     for (int i = 3; i * i <= mx; i += 2) {
-        if (sieve[i]) {
+        if (prime[i]) {
             for (int j = i * i; j <= mx; j += i) {
-                sieve[j] = false;
+                prime[j] = false;
             }
         }
     } 
 }
-
+ 
 void solve() {
     vector<int> primes;
     primes.push_back(2);
     for (int i = 3; i <= mx; i += 2) {
-        if (sieve[i]) {
+        if (prime[i]) {
             primes.push_back(i);
         }
     }
@@ -35,11 +35,11 @@ void solve() {
         cout << primes[i] << endl;
     }
 }
-
+ 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    primeSieve();
+    sieve();
     solve();
     return 0;
 }
